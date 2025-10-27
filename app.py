77ab6722ca91f0f463473440036a4bf8188074b4
amphibiustech <amphibiustech@gmail.com>
2025-10-27 17:35:05 +0530

    images and template added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
+from src.database.db_json import category
 
 app = Flask(__name__)
 
@@ -6,5 +7,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/category/<name>')
+def category_details(name):
+    category_name = category[name]
+    return render_template('card.html', category=category_name)
+ 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ src/database/db_json.py
@@ -0,0 +1,112 @@
+category = {
+    "vegetables": [
+        {
+            "name": "Tomatoes",
+            "weight": "1 kg",
+            "cost_per_weight": 40,
+            "image_url": "/static/images/tomatoes.jpg"
+        },
+        {
+            "name": "Green Chillies",
+            "weight": "100 gm",
+            "cost_per_weight": 20,
+            "image_url": "/static/images/chillies.jpg"
+        },
+        {
+            "name": "Onions",
+            "weight": "1 kg",
+            "cost_per_weight": 35,
+            "image_url": "/static/images/onions.jpg"
+        },
+        {
+            "name": "Potatoes",
+            "weight": "1 kg",
+            "cost_per_weight": 30,
+            "image_url": "/static/images/potatoes.jpg"
+        }
+    ],
+
+    "fruits": [
+        {
+            "name": "Banana",
+            "weight": "1 dozen",
+            "cost_per_weight": 50,
+            "image_url": "/static/images/banana.jpg"
+        },
+        {
+            "name": "Red Apples",
+            "weight": "1 kg",
+            "cost_per_weight": 120,
+            "image_url": "/static/images/apple.jpg"
+        },
+        {
+            "name": "Oranges",
+            "weight": "1 kg",
+            "cost_per_weight": 80,
+            "image_url": "/static/images/oranges.jpg"
+        }
+    ],
+
+    "dairy": [
+        {
+            "name": "Milk",
+            "weight": "1 L",
+            "cost_per_weight": 60,
+            "image_url": "/static/images/milk.jpg"
+        },
+        {
+            "name": "Paneer",
+            "weight": "200 gm",
+            "cost_per_weight": 80,
+            "image_url": "/static/images/paneer.jpg"
+        },
+        {
+            "name": "Curd",
+            "weight": "500 gm",
+            "cost_per_weight": 40,
+            "image_url": "/static/images/curd.jpg"
+        }
+    ],
+
+    "groceries": [
+        {
+            "name": "Basmati Rice",
+            "weight": "1 kg",
+            "cost_per_weight": 180,
+            "image_url": "/static/images/rice.jpeg"
+        },
+        {
+            "name": "Wheat",
+            "weight": "1 kg",
+            "cost_per_weight": 50,
+            "image_url": "/static/images/wheat.jpg"
+        },
+        {
+            "name": "Toor Dal",
+            "weight": "1 kg",
+            "cost_per_weight": 120,
+            "image_url": "/static/images/toor-dal.jpg"
+        }
+    ],
+
+    "snacks": [
+        {
+            "name": "Potato Chips",
+            "weight": "50 gm",
+            "cost_per_weight": 20,
+            "image_url": "/static/images/chips.jpg"
+        },
+        {
+            "name": "Biscuits",
+            "weight": "200 gm",
+            "cost_per_weight": 30,
+            "image_url": "/static/images/biscuit.jpg"
+        },
+        {
+            "name": "Namkeen",
+            "weight": "250 gm",
+            "cost_per_weight": 40,
+            "image_url": "/static/images/namkeen.jpg"
+        }
+    ]
+}
